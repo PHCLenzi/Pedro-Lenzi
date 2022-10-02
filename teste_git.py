ff2a4636a580465f9664524770aee04af575822b
PHCLenzi <pedrohclenzi@gmail.com>
2022-10-02 15:50:23 +0200

    local B1 6

--- teste_git.py
+++ teste_git.py
@@ -1,9 +1,11 @@
 import numpy
 
 # teste de git
-# teste local 2
+# teste local 2 b481aa83fd624ac615c14660eeda01103df6b6a1
 # teste remote 3
-# teste local 4
-# teste local B1 5
+# teste local 4 4ae083870d58476291a008c3c6580975cca13ab7 
+# teste local B1 5 059da56c572cba81cdd284127f90d46f77cc825d
+# teste local B1 6
+
 
 
