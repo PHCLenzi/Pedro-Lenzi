4c6362a1b62d2c2ca85f06acc27c9bce76c7ed8d
PHCLenzi <pedrohclenzi@gmail.com>
2022-10-04 18:07:41 +0200

    mensagem deletei tudo

--- teste_git.py
+++ teste_git.py
@@ -1,26 +1,3 @@
 import numpy
 
-# teste de git
-# teste local 2 b481aa83fd624ac615c14660eeda01103df6b6a1
-# teste remote 3
-# teste local mater 6
-# teste local mater 7
-# teste local 4
-# teste local B1 5
-# teste local 4 4ae083870d58476291a008c3c6580975cca13ab7 
-# teste local B1 5 059da56c572cba81cdd284127f90d46f77cc825d
-# teste local B1 6
-# teste local mater 8
 
-# teste local mater 9
-# teste local 4 4ae083870d58476291a008c3c6580975cca13ab7 
-# teste local B1 5 059da56c572cba81cdd284127f90d46f77cc825d
-# teste local B1 6
-
-# teste B1 voltou para master
-
-# teste master 10 remote
-# teste master 11 local
-# teste local inicio B2
-# teste local B2 2
-# teste local master 12
